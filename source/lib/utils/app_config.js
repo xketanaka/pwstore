aa885fb5508b97a10af4881f11b7c626c42cb758
tanaka ken <tanaka-ken@netlab.jp>
2017-07-31 11:32:40 +0900

    profileごとにDBを保持できるように修正

--- source/lib/utils/app_config.js
+++ source/lib/utils/app_config.js
@@ -4,11 +4,13 @@ const electron = require("electron");
 const Config = require("./config");
 
 class AppConfig {
-  static initialize(iniFilePath){
+  static initialize(iniFilePath, profile = "default"){
     this._config = JSON.parse(new Buffer(fs.readFileSync(iniFilePath, 'utf8'), 'hex').toString('utf8'));
+    this._profile = profile;
     Config.initialize(this);
   }
   static get appKey(){ return this._config.appKey }
+  static get profile(){ return this._profile }
   static get padding(){ return crypto.randomBytes(this._config.appKey.length).toString("hex") }
   static get userDataDir(){ return electron.app.getPath('userData') }
 }
--- source/main.js
+++ source/main.js
@@ -1,4 +1,9 @@
-const config = require("./lib/utils/app_config").initialize(`${__dirname}/config/app.ini`);
+const isDev = process.defaultApp || /node_modules[\\/]electron[\\/]/.test(process.execPath);
+const argv = process.argv.slice(isDev ? 2 : 1);
+
+let profile = argv.reduce((prof, v)=>{ return prof ? (v == "--profile" ? undefined : prof) : v }, "default");
+
+const config = require("./lib/utils/app_config").initialize(`${__dirname}/config/app.ini`, profile);
 const pwstore = require("./lib/pwstore");
 
 process.on('uncaughtException', (error)=>{
